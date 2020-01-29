f87ebdc8df533835a3edb164d61578ca5a1d56b5
AJ Speller <ajspeller@gmail.com>
2020-01-29 02:27:19 -0500

    Added the shared margin-icon component

---
+++ src/app/shared/margin-icon/margin-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-margin-icon',
+  templateUrl: './margin-icon.component.html',
+  styleUrls: ['./margin-icon.component.css']
+})
+export class MarginIconComponent implements OnInit {
+  @Input() iconName: string;
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MarginIconComponent } from './margin-icon/margin-icon.component';
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [ToolbarComponent, MarginIconComponent],
   imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [ToolbarComponent]
+  exports: [ToolbarComponent, MarginIconComponent]
 })
 export class SharedModule {}
