165d0b369b19c723a1145720192e25c621038a7c
AJ Speller <ajspeller@gmail.com>
2020-01-29 02:36:12 -0500

    Changed default namespace to fpa

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-toolbar',
+  selector: 'fpa-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
