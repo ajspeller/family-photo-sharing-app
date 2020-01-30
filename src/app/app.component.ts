1b69c2e3d4a18c312ab3a3807cf235004a5c850b
AJ Speller <ajspeller@gmail.com>
2020-01-29 23:01:58 -0500

    Add code to listen for screensize changes using FlexLayout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'fpa-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   navBarOpen = true;
   routes = [
     { path: '/', title: 'Home', icon: 'home' },
     { path: '/albums', title: 'Albums', icon: 'folder' },
     { path: '/users', title: 'User', icon: 'person' }
   ];
+  watcher: Subscription;
+
+  constructor(mediaObserver: MediaObserver) {
+    this.watcher = mediaObserver
+      .asObservable()
+      .pipe(
+        filter((changes: MediaChange[]) => changes.length > 0),
+        map((changes: MediaChange[]) => changes[0])
+      )
+      .subscribe((change: MediaChange) => {
+        if (change.mqAlias === 'xs') {
+          this.loadMobileContent();
+        } else {
+          this.loadDashboardContent();
+        }
+      });
+  }
+
+  loadDashboardContent() {
+    console.log('large view');
+    // Do something special since the viewport is currently
+    // using large display sizes
+  }
+
+  loadMobileContent() {
+    console.log('small view');
+    // Do something special since the viewport is currently
+    // using mobile display sizes
+  }
 
   toggleNav() {
     this.navBarOpen = !this.navBarOpen;
+  }
+
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
   }
 }
